4d4430d94527f3e339940e1c5938c2aece072701
Dwiferdio Seagal Putra <dwiferdio.putra@alodokter.com>
2024-02-22 16:17:16 +0700

    implement a REPL using buffered input

--- main.c
+++ main.c
@@ -1,25 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 
 #include "parser.h"
 
-void run_file(char* file_path) {
-	printf("File path -> %s", file_path);
+#define RL_BUFSIZE 1024
+
+void exit_handler(int signum) {
+  exit(signum);
 }
 
-void run_prompt() {}
+void run_file(char* file_path) {
+  printf("File path -> %s\n", file_path);
+}
+
+char* read_line() {
+  int bufsize = RL_BUFSIZE;
+  int pos = 0;
+  int c;
+  char* buff = malloc(sizeof(char) * bufsize);
+
+  if (!buff) {
+    fprintf(stderr, "lox: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    c = getchar();
+
+    if (c == EOF || c == '\n') {
+      buff[pos] = '\0';
+
+      return buff;
+    } else {
+      buff[pos] = c;
+    }
+
+    pos++;
+
+    if (pos >= bufsize) {
+      // incline the size of the buffer
+      bufsize += RL_BUFSIZE;
+      buff = realloc(buff, bufsize);
+
+      if (!buff) {
+        fprintf(stderr, "lox: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
+
+void run_prompt() {
+  // do loop for getting user input
+  char* buff;
+  
+  do {
+    printf("lox> ");
+    buff = read_line();
+
+    printf("User input -> %s\n", buff);
+
+    free(buff);
+  } while (*buff == EOF || signal(SIGTERM, exit_handler) == SIG_ERR || buff == "exit()");
+}
 
 int main(int argc, char *argv[]) {
-	if (argc > 2) {
-		printf("Usage: clox [script]");
-		return 64;
-	} else if (argc == 2) {
-		// running a lox file
-		char* path = argv[1];
-		run_file(path);
-	} else {
-		// spawn a REPL or execute a code immidiately
-		run_prompt();
-	}
-	
-	return 0;
+  if (argc > 2) {
+    printf("Usage: clox [script]\n");
+    return 64;
+  } else if (argc == 2) {
+    // running a lox file
+    char* path = argv[1];
+    run_file(path);
+  } else {
+    // spawn a REPL or execute a code immidiately
+    run_prompt();
+  }
+  
+  return EXIT_SUCCESS;
 }
